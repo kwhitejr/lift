90db6641bcb0bcc57483f6be49dcca00d2ef749c
Kevin White <kwhitejr@gmail.com>
2022-12-18 14:14:21 -0800

    broken ass shit

--- lift/src/App.js
+++ lift/src/App.js
@@ -1,14 +1,19 @@
+import Calendar from "./Calendar";
+import data from "./calendar-demo-data.json";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Calendar
+        data={data}
+        config={{
+          x: d => d["Date"],
+          y: d => d.Volume,
+        }}
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
